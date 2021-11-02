8ce2ad96f0df1f5d8cd4307efd389e359d406ae0
Christian Pereira <christianpbsd@gmail.com>
2021-11-02 17:01:47 -0300

    get Photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import * as C from './App.styles';
+import * as Photos  from './services/photos';
+import { useState, useEffect } from 'react';
+import { Photo } from './types/Photo';
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [ photos, setPhotos ] = useState<Photo[]>([]);
+
+  useEffect(() => {
+
+    const getPhotos = async () => {
+      setLoading(true);
+        setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, []);
+
   return(
     <C.Container>
         <C.Area>
@@ -9,6 +26,21 @@ const App = () => {
             {/* Área de upload */}
 
             {/* Lista de Fotos */}
+
+            {loading &&
+            <C.ScreenWarning>
+                <div className="emoji">✋</div>
+                <div>Carregando...</div>
+            </C.ScreenWarning>
+            }
+
+            {!loading && photos.length > 0 &&
+              <C.PhotoList>
+                  {photos.map((item, index) => (
+                    <div>{item.name}</div>
+                  ))}
+              </C.PhotoList>
+            }
 
         </C.Area>
     </C.Container>
