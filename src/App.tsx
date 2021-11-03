2d2412b1f731fa4845a8a1b8b389955e84a0c4ef
Christian Pereira <christianpbsd@gmail.com>
2021-11-02 21:05:45 -0300

    Photo list

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as C from './App.styles';
 import * as Photos  from './services/photos';
 import { useState, useEffect } from 'react';
 import { Photo } from './types/Photo';
+import { PhotoItem } from './components/PhotoItem';
+
 
 const App = () => {
 
@@ -37,9 +39,16 @@ const App = () => {
             {!loading && photos.length > 0 &&
               <C.PhotoList>
                   {photos.map((item, index) => (
-                    <div>{item.name}</div>
+                    <PhotoItem key={index} url={item.url} name={item.name} />
                   ))}
               </C.PhotoList>
+            }
+
+            {!loading && photos.length === 0 &&
+              <C.ScreenWarning>
+              <div className="emoji">😞</div>
+              <div>Não há fotos cadastradas!</div>
+          </C.ScreenWarning>
             }
 
         </C.Area>
